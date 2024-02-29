28ef52ede27911121774638117d8a52f96266d2c
ji4704 <ji4704@naver.com>
2024-02-29 15:30:07 +0900

    change

--- studyJava/src/Main.java
+++ studyJava/src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-        System.out.println("메타버스 에듀 태크 개발자 과정 4회차");
-
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
 }
