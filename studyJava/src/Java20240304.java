8dec8ffbdccd6b0eb65ee651842c523c65a1c1d3
ji4704 <ji4704@naver.com>
2024-03-04 15:33:11 +0900

    change

--- studyJava/src/Java20240304.java
+++ studyJava/src/Java20240304.java
@@ -93,6 +93,38 @@ public class Java20240304 {
         while (count < 5) {
             System.out.print(count + " ");
             count++; // 증감식
+            System.out.println(
+
+            );
+
+            //의도적인 무한루프에서 while문은 자주 쓰임
+//            while (true) {
+//                // 무한루프
+//                // 유저가 버튼 클릭할때까지 대기
+//                // if (esc버튼 클릭)
+//                //      break;
+//            }
+        }
+        // do ~ while 문
+        int cnt1 = 0; // 초기식
+        do {
+            System.out.print(cnt1 + " ");
+            cnt1++;
+        } while (cnt1 < 5);
+        // ㅏ래 while 문과 비교하면,
+        // 반목문의 조건이 처음부터 false인 경우,
+        // do~while문은 실행코드가 1회 싱행되는 반면, while문은 실행되지 않음
+        int cnt2 = 5;
+        while (cnt2 < 5) {
+            System.out.print(cnt2 + " ");
+            cnt2++;
+        }
+
+        // 이중루프, 이중반복문
+        for (int g=0; g<5; g++) {
+            for (int j=0; j<6; j++) {
+                System.out.println("[g=" + g + ", j=" + j + "j");
+            }
         }
     }
 }
