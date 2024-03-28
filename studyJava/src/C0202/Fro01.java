35cbf6b086ef5f3169b4348052b3482053d5836d
ji4704 <ji4704@naver.com>
2024-03-28 17:29:19 +0900

    change

---
+++ studyJava/src/C0202/Fro01.java
@@ -0,0 +1,25 @@
+package C0202;
+
+import Java0328.Main;
+
+public class Fro01 {
+    public static void main(String[] args) {
+
+        System.out.println(5 < 2); // false
+        System.out.println(5 > 2); // true
+        System.out.println(5 < 5); // false
+        System.out.println(5 <= 5); // true
+        System.out.println(5 >= 5); // true
+        System.out.println();
+
+        int a = 5;
+        int b = 2;
+        int c = 5;
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a == c); // true
+        System.out.println(a != c); // flase
+        System.out.println();
+
+    }
+}
