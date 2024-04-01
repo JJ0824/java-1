28ca507a196ea82c565967ffe9e739ab8c4c47a0
ji4704 <ji4704@naver.com>
2024-04-01 22:02:56 +0900

    0401

---
+++ studyJava/src/StudyGroup/Study0401/TimeAttack.java
@@ -0,0 +1,63 @@
+package StudyGroup.Study0401;
+/*
+사용자에게 1~10까지의 수 중 하나를 입력받는다.
+(조건1. 입력받은 값이 1~10의 범위를 넘어간 경우 Error 출력)///////////
+사용자가 입력한 값을 userInput으로 받은 뒤/////////////
+그 수가 짝수인 경우 입력 받은 수 부터 10까지의 숫자를 모두 더한 값을 출력하고,/////////////
+그 숫자가 홀수인지 짝수 인지 출력하는 코드를 짜시오. /////////////////////////////////////////
+또한 사용자가 입력한 값이 홀 수 인 경우 /////////////////////////////////////
+입력 받은 수부터 10까지의 숫자를 모두 곱한 값을 출력하고,
+그 숫자가 홀수인지 짝수 인지 출력하는 코드를 짜시오.
+
+(조건2. 계산 결과값이 20보다 클 경우)
+"결과값은 20보다 큰 수 입니다"
+작을 경우
+"결과값은 20보다 작은 수 입니다."
+를 출력하시오.
+*/
+public class TimeAttack {
+    public static void main(String[] args) {
+
+        int num =0;
+        int  userInput = 5;
+        int mul=1;
+
+
+        if(userInput % 2==1){
+
+            for(int j = userInput; j<=10; j++) {
+
+                mul = num * j;
+            }
+            System.out.println(num);
+
+        }
+        if (userInput % 2==0){
+            for(int i = userInput; i<=10; i++) {
+
+                num = num + i;
+            }
+            System.out.println(num);
+        }
+
+        int a = 1;
+        if (num % 2 == 0) {
+            a = a +1;
+        }
+        switch (a) {
+            case 1:
+                System.out.println("홀수");
+                break;
+            case 2:
+                System.out.println("짝수");
+                break;
+
+
+
+        }
+
+//        else {
+//            System.out.println("Error");
+        }
+    }
+
