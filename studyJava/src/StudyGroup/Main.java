6d68b9f38f0b27b8bfdc731c1e85f3dd57062e9e
ji4704 <ji4704@naver.com>
2024-04-16 20:28:46 +0900

    c

--- studyJava/src/StudyGroup/Main.java
+++ studyJava/src/StudyGroup/Main.java
@@ -1,4 +1,18 @@
 package StudyGroup;
 
+import java.util.Scanner;
+// 사용자가 입력한 숫자를 찾는 코드를 작성하시오.
 public class Main {
+    public static void main(String[] args) {
+        long count = 0;
+        Scanner scanner = new Scanner(System.in);
+        long userNum = scanner.nextInt();
+        for (long i = 0; ; i++) {
+            count++;
+            if (i == userNum) {
+                System.out.println("유저가 입력한 값은 [ " + i + " ] 입니다." + count);
+                break;
+            }
+        }
+    }
 }
