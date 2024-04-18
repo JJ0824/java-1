5e6ba7f11e8d1e90ae7f1929d8bf6b468c6601a3
ji4704 <ji4704@naver.com>
2024-04-18 21:41:11 +0900

    c

--- studyJava/src/StudyGroup/Study0417/HW01.java
+++ studyJava/src/StudyGroup/Study0417/HW01.java
@@ -2,23 +2,24 @@ package StudyGroup.Study0417;
 
 import java.util.Scanner;
 
+// 사용자로부터 입력받은 n단까지의 구구단을출력하는 코드를 작성하시오.
 public class HW01 {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.print("몇 단까지 출력 할까요? : ");
-        int input = scn.nextInt();
+
+        System.out.println("몇단까지 알고싶은지 숫자를 쓰시오: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
         int a = 0;
 
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                if (input >= i) {
+        for (int i = 1; ; i++) {
+            if (i > num) {
+                break;
+            } else {
+                for (int j = 1; j < 10; j++) {
                     a = i * j;
                     System.out.print(a + " ");
-                    if(j==9){
-                        System.out.println();
-                    }
                 }
-            }
+            }System.out.println();
         }
     }
 }
