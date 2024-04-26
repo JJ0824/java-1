2fc755ebf0681b5777ffe414a7607c31b3429fe7
ji4704 <ji4704@naver.com>
2024-04-27 01:31:07 +0900

    c

---
+++ studyJava/src/StudyGroup/Study0426/HW03.java
@@ -0,0 +1,22 @@
+package StudyGroup.Study0426;
+//구구단 8단의 코드를 작성하시오.
+//출력 예시 :
+//8 * 1 = 8
+//8 * 2 - 16
+// ...
+public class HW03 {
+    public static void main(String[] args) {
+
+        System.out.println("8" + " * " + "1" + " = " + (8 * 1));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 2));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 3));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 4));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 5));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 6));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 7));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 8));
+        System.out.println("8" + " * " + "1" + " = " + (8 * 9));
+
+
+    }
+}
