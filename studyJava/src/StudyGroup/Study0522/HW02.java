68ec55197f387dd74742f92e79155e0e270ecea6
ji4704 <ji4704@naver.com>
2024-05-23 00:13:15 +0900

    d

---
+++ studyJava/src/StudyGroup/Study0522/HW02.java
@@ -0,0 +1,32 @@
+
+package StudyGroup.Study0522;
+
+import java.util.Scanner;
+
+// 공통 조건 : 3개의 과제 클래스에서 필요한 외부 클래스는 하나의 클래스 NumberForHw로 통일하여 사용
+public class HW02 {
+    //사용자에게 입력받은 숫자가 1의 자리인지 10의 자리인지 100의 자리인지 알아내는 코드를 작성하시오.
+    //입력매게변수 사용
+    public static void main(String[] args) {
+
+        int[] array = new int[100];
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 0; i<array.length; i++) {
+            array[i] = scanner.nextInt();
+            if (array[i] < 10) {
+                System.out.println("1의 자리 숫자입니다.");
+            } else if (array[i] <= 99) {
+                System.out.println("10의 자리 숫자입니다.");
+            } else if (array[i] == 100) {
+                System.out.println("100의 자리 숫자입니다.");
+            }else {
+
+            }
+            break;
+        }
+
+    }
+}
