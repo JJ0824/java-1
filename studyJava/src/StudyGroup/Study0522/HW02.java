c9f302a1abf6d14ad43c8dcd037d4854be2837f9
ji4704 <ji4704@naver.com>
2024-05-23 19:47:41 +0900

    ;lm

--- studyJava/src/StudyGroup/Study0522/HW02.java
+++ studyJava/src/StudyGroup/Study0522/HW02.java
@@ -7,25 +7,21 @@ import java.util.Scanner;
 public class HW02 {
     //사용자에게 입력받은 숫자가 1의 자리인지 10의 자리인지 100의 자리인지 알아내는 코드를 작성하시오.
     //입력매게변수 사용
+    // 배열이 필요가없고 for문도 필요가 없다 (반복을 안하기떄문에)
     public static void main(String[] args) {
 
-        int[] array = new int[1];
-
         Scanner scanner = new Scanner(System.in);
-
-
-        for (int i = 0; i<array.length; i++) {
-            array[i] = scanner.nextInt();
-            if (array[i] < 10) {
+        int a = scanner.nextInt();
+            if (a < 10) {
                 System.out.println("1의 자리 숫자입니다.");
-            } else if (array[i] <= 99) {
+            } else if (a <= 99) {
                 System.out.println("10의 자리 숫자입니다.");
-            } else if (array[i] <= 999) {
+            } else if (a <= 999) {
                 System.out.println("100의 자리 숫자입니다.");
-            } else {
+            } else{
                 System.out.println("범위를 벗어났습니다.");
             }
-        }
+
 
     }
 }
