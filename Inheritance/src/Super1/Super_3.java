a190afb37fd778160bfcd5179de246a1848c7907
JJ0824 <auxjungjae@gmail.com>
2024-11-08 12:17:18 +0900

    d

--- Inheritance/src/Super1/Super_3.java
+++ Inheritance/src/Super1/Super_3.java
@@ -20,9 +20,9 @@ class B1 extends A1 {
 }
 public class Super_3 {
     public static void main(String[] args) {
-//        A1 value1 = new A1();
-//        A1 value2 = new A1(3);
-//        B1 value3 = new B1();
-        B1 value4 = new B1(3);
+//        A1 value1 = new A1(); // A1생성자 1
+//        A1 value2 = new A1(3); // A1생성자 2
+        B1 value3 = new B1(); // A1생성자 2, A1생성자 1, B1생성자 2, B1생성자 1
+//        B1 value4 = new B1(3); // A1생성자 2, A1생성자 1, B1생성자 2
     }
 }
